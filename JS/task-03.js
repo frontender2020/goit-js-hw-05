556a5471747b1f2dd626e9181bceec1d90a623d1
Victor Romanov <germanist2008@yandex.ru>
2020-02-22 20:13:25 +0200

    tasks solved

---
+++ JS/task-03.js
@@ -0,0 +1,33 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem() {
+    return this.items.push("Дроид");
+  }
+
+  removeItem() {
+    return this.items.splice(1, 1);
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
